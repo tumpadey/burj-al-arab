0c55b07e492728622402c9a87fc04b2065e17fb4
tumpadey <tumpadey20000@gmail.com>
2020-10-27 13:29:06 +0600

    done

---
+++ src/components/Login/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEO_DX1g-IIxA7B-TVJjBNdfQZH6fJPC8",
+    authDomain: "burj-al-arab-c15d1.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-c15d1.firebaseio.com",
+    projectId: "burj-al-arab-c15d1",
+    storageBucket: "burj-al-arab-c15d1.appspot.com",
+    messagingSenderId: "548386458728",
+    appId: "1:548386458728:web:53c73f7f88e1c8091e3fdd"
+  };
+  export default firebaseConfig;
